56d5e800b8a6de1c715ff8215ad2e78693194b7a
Dizrtt <demonic2003@bk.ru>
2023-02-15 21:16:34 +0200

    Initial commit.

---
+++ HW_Task.java
@@ -0,0 +1,17 @@
+//Написать метод который приводит сторку к нормальному виду
+// строка на вход
+// Добрый    день  Как   дела Все    хорошо
+//выход
+// Добрый день. Как дела. Все хорошо.
+
+public class HW_Task {
+    public static void main(String[] args) {
+
+    }
+
+    public static String makeStringBetter(String) {
+        
+        return;
+    }
+
+}
